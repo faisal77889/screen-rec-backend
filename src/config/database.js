0be29ed1ff4e488d6c39cdd1f71c42758d5652e1
Syed Faisal Hassan <faisalhassan77889@gmail.com>
2025-05-20 00:58:51 +0530

    mongo db uri

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect("mongodb+srv://faisalhassan77889:Fai123sal123@loom-recorder.42mclwn.mongodb.net/?retryWrites=true&w=majority&appName=loom-recorder/backend")
+        await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.log("Something went wrong " + error.message)
     }
